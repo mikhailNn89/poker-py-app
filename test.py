15ddf68154a0bd7264acedf7707dcf348215fa27
Mikhail Shashanov <33182872+mikhailNn89@users.noreply.github.com>
2018-10-07 22:55:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,6 @@ class TestBlank(unittest.TestCase):
     def test1(self):
         print('Blank test 1')
         self.assertEqual(20,20)
+        
+if __name__ == '__main__':
+    unittest.main()        

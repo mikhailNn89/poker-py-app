c64e2f57b7c6ee2082a5abc797740212fb3e205c
Mikhail Shashanov <33182872+mikhailNn89@users.noreply.github.com>
2018-10-09 18:55:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import unittest
 import sys
 
+def assertTest():
+    print('Test #0 beginning')
+    self.assertEqual(20,20)
+
 class Test1(unittest.TestCase):
     def setUp(self):
         print('Test #1 before')

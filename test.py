77a34866f64910cffa999dad2384a4295f4cb52d
Mikhail Shashanov <33182872+mikhailNn89@users.noreply.github.com>
2018-10-11 19:11:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,24 +5,24 @@ from t_class import *
 
 # Arseny: did you run this? What is 'self'? What for is this function?
 def test_myTest():
-    print('Test #0 beginning')
+    print('Test #0')
     self.assertEqual(20,20)
 
 class Test1(unittest.TestCase):
     def setUp(self):
-        print('Test before\n')
+        print('Test before')
     
     def test1(self):
-        print('Test #1 beginning')
+        print('Test #1')
         self.assertEqual(20,20)
         
     def test2(self):
-        print('Test #2 beginning')
+        print('Test #2')
         obj = TestedClass()
         self.assertEqual(obj.foo(),7)    
         
     def tearDown(self):
-        print('Test end\n')    
+        print('Test end')    
         
 if __name__ == '__main__':
     unittest.main()        

bd7d7ddabacbff45bee97f67a96ffd6b296ce883
Andrey A Molkov <molkov_aa@mail.ru>
2018-10-13 16:24:09 +0300

    gui test

--- Table_main.py
+++ Table_main.py
@@ -1,117 +1,170 @@
 from tkinter import Tk, FALSE, Button
 import tkinter
-from tkinter import *
-from tkinter.messagebox import *
 from Table_py import *
 from PIL import Image, ImageTk #pip install pillow
-from time import sleep
+import os
 
-form1 = tkinter.Tk()
-form1.title('form1')
-form1.resizable(width=FALSE, height=FALSE)
-form1.geometry('1300x662+100+100')
-image = Image.open("image//suit.jpg")
-suit = ImageTk.PhotoImage(image)
+class GUI:
+    def __init__(self, form):
+        THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+        self.form1 = form
+        self.form1.title('form1')
+        self.form1.resizable(width=FALSE, height=FALSE)
+        self.form1.geometry('1300x662+100+100')
+        self.weigh_card = 0.046
+        self.height_card = 0.13
+        self.user_name = tkinter.Entry(self.form1, textvariable=b'YOUR NAME')
+        self.user_name.place(relx=0.20, rely=0.05, relwidth=0.07, relheight=0.03)
+        self.user_ip = tkinter.Entry(self.form1, textvariable='IP')
+        self.user_ip.place(relx=0.30, rely=0.05, relwidth=0.06, relheight=0.03)
+        self.server_port = tkinter.Entry(self.form1, textvariable='PORT')
+        self.server_port.place(relx=0.40, rely=0.05, relwidth=0.04, relheight=0.03)
+        self.user_connect = Button(text='connect', command=connect_click)
+        self.user_connect.place(relx=0.45, rely=0.05, relwidth=0.04, relheight=0.03)
+        self.cards_center = []
+        self.bank = Button(text='bank of casino')
+        self.croupier = {'bank': self.bank, 'cards': self.cards_center}
+        self.user_name = {'1': 'player 1', '2': 'user 2', '3': 'player 3', '4': 'player 4', '5': 'player 5', '6': 'player 6',
+                     '7':'player 7'}
 
-buf1 = StringVar()
-buf2 = StringVar()
-buf3 = StringVar()
-buf1.set('YOUR NAME')
-user_name=server_port = tkinter.Entry(form1,textvariable=buf1)
-user_name.place(relx=0.20, rely=0.05, relwidth=0.07, relheight=0.03)
-buf2.set('IP')
-user_ip=server_port = tkinter.Entry(form1,textvariable=buf2)
-user_ip.place(relx=0.30, rely=0.05, relwidth=0.06, relheight=0.03)
-buf3.set('PORT')
-server_port = tkinter.Entry(form1,textvariable=buf3)
-server_port.place(relx=0.40, rely=0.05, relwidth=0.04, relheight=0.03)
-user_connect = Button(text='connect', command=connect_click)
-user_connect.place(relx=0.45, rely=0.05, relwidth=0.04, relheight=0.03)
+        # name of cards 'suit', 'ace', 'king', 'queen', 'jack', '10', '9', '8', '7', '6', '5', '4''3''2''j_red', 'j_black'
+        # suit of cards (FOLDERS) -'suit', 'joker', 'club', 'diamond', 'heart', 'spade'
+        self.shirt = ""
+        self.store_image = []
+        self.player_places = []
+
+    def get_image(self, suit, name):
+        self.store_image.append(ImageTk.PhotoImage(Image.open(os.path.join('image', 'cards', suit, name + '.jpg'))))
+        return self.store_image[len(self.store_image)-1]
+
+    def set_card_player(self, number,):
+            pass
+
+    def update_bank_croupier(self, data):
+        self.croupier.get('bank')['text'] = data
+
+    def add_card_on_center(self, suit, name):
+        #last number is start 'x'
+        delta = (len(self.croupier.get('cards')) * 0.03) + 0.25
+        self.bank.place(relx=0.3, rely=0.25, relwidth=0.09, relheight=0.03)
+        card_server = (Button(text='card'))
+        card_server.place(relx=delta, rely=0.31, relwidth=self.weigh_card, relheight=self.height_card)
+        card_server.config(image=self.get_image(suit, name))
+        (self.croupier.get('cards')).append(card_server)
+        return self.croupier
+
+    def place_for_user(self, user, x, y):
+        bank = Button(text="0")
+        bank.place(relx=x, rely=y, relwidth=0.09, relheight=0.03)
+
+        card1 = Button(text='card1', image=self.shirt)
+        card1.place(relx=x, rely=y + 0.04, relwidth=self.weigh_card, relheight=self.height_card)
+
+        card2 = Button(text='card2', image=self.shirt)
+        card2.place(relx=x + 0.05, rely=y + 0.04,  relwidth=self.weigh_card, relheight=self.height_card)
+
+        bet_bank = Button(text='0', state='disable')
+        bet_bank.place(relx=x + 0.11, rely=y + 0.02, relwidth=0.04, relheight=0.03)
+
+        raise_bet = Button(text='raise bet', command=raise_bet_click, state='disable')
+        raise_bet.place(relx=x + 0.11, rely=y + 0.06, relwidth=0.04, relheight=0.03)
+
+        pass_button = Button(text='pass', command=pass_button_click, state='disable')
+        pass_button.place(relx=x + 0.11, rely=y + 0.1, relwidth=0.04, relheight=0.03)
+
+        check = Button(text='check', command=check_click, state='disable')
+        check.place(relx=x + 0.11, rely=y + 0.14, relwidth=0.04, relheight=0.03)
+
+        user = Button(text='user ' + user)
+        user.place(relx=x, rely=y + 0.19, relwidth=0.09, relheight=0.03)
+
+        return {'bank': bank, 'raise_bet': raise_bet, 'bet': bet_bank, 'card1': card1,
+                'card2': card2, 'check': check, 'user': user, 'pass': pass_button}
+
+    def setting_player_places(self):
+        x_default = 0.02
+        y_default = 0.75
+        user_name = my_gui.user_name
+        self.player_places.append(my_gui.place_for_user(user_name.get('1'), x_default, y_default - 0.6))
+        self.player_places.append(my_gui.place_for_user(user_name.get('2'), x_default, y_default - 0.3))
+        self.player_places.append(my_gui.place_for_user(user_name.get('3'), x_default, y_default))
+        self.player_places.append(my_gui.place_for_user(user_name.get('4'), x_default + 0.3, y_default))
+        self.player_places.append(my_gui.place_for_user(user_name.get('5'), x_default + 0.6, y_default))
+        self.player_places.append(my_gui.place_for_user(user_name.get('6'), x_default + 0.6, y_default - 0.3))
+        self.player_places.append(my_gui.place_for_user(user_name.get('7'), x_default + 0.6, y_default - 0.6))
+
+    def set_card_one_player(self, position, suit1, name_card1, suit2, name_card2):
+        my_gui.player_places[1].get('card1').config(image=my_gui.get_image(suit1, name_card1))
+        my_gui.player_places[1].get('card2').config(image=my_gui.get_image(suit2, name_card2))
+
+    def change_croupier(self, bank, suit, name_card):
+        self.croupier.get('bank')['text'] = bank
+        self.add_card_on_center(suit, name_card)
+        return self.croupier
+
+    def change_button_player(self, position, flag):
+        #disabled on default setting
+        status = 'disable'
+        if flag == TRUE:
+            status = 'active'
+
+        (self.player_places[position].get('bet'))['state'] = status
+        (self.player_places[position].get('raise_bet'))['state'] = status
+        (self.player_places[position].get('pass'))['state'] = status
+        (self.player_places[position].get('check'))['state'] = status
+
+    def change_bet_player(self,position, bet):
+        bank = (self.player_places[position].get('bank'))['text']
+        if (int(bank) - int(bet)) > 0:
+            (self.player_places[position].get('bet'))['text'] = bet
+        else:
+            bet = bank
+            (self.player_places[position].get('bet'))['text'] = bet
+
+        (self.player_places[position].get('bank'))['text'] = bank
+        if int(bank) > 0:
+            (self.player_places[position].get('bank'))['text'] = (int(bank) - int(bet))
+
+    def add_bank_player(self,position, summa):
+        if summa > 0:
+            (self.player_places[position].get('bank'))['text'] = summa
+
+    def change_name_user(self,position, name):
+        (self.player_places[position].get('user'))['text'] = name
 
 
-def add_card_on_center(master, delta):
-        bank = Button(text='bank of casino')
-        bank.place(relx=0.3, rely=0.25, relwidth=0.09, relheight=0.03)
-        card_server = Button(text='card')
-        card_server.place(relx=delta+0.02, rely=0.31, relwidth=0.04, relheight=0.13)
-        cards_center.append(card_server)
-        return cards_center
+root = Tk()
+my_gui = GUI(root)
+my_gui.shirt = my_gui.get_image('shirt', 'shirt')
+my_gui.setting_player_places()
+
+# name of cards 'suit', 'ace', 'king', 'queen', 'jack', '10', '9', '8', '7', '6', '5', '4''3''2''j_red', 'j_black'
+# suit of cards (FOLDERS) -'suit', 'joker', 'club', 'diamond', 'heart', 'spade'
+
+# case of play
+
+#change cards of first player
+my_gui.set_card_one_player(1, 'diamond', 'queen', 'club', '2')
+#add card from croupie
+cards_center = my_gui.change_croupier(100, 'joker', 'j_red')
+#add card from croupie
+cards_center = my_gui.change_croupier(1000, 'club', 'jack')
+#change state of button first player
+my_gui.change_button_player(1, TRUE)
+#my_gui.change_button_player(1, FALSE) # example for disabled button for a player
+
+#cases of player
+my_gui.change_name_user(1, "boris")
+
+my_gui.add_bank_player(1, 100)
+
+my_gui.change_bet_player(1, 10)
 
 
-def place_for_user(user, x, y):
-    #suit = ImageTk.PhotoImage('lena.ppm')
-    bank = Button(text='bank of '+user)
-    bank.place(relx=x, rely=y, relwidth=0.09, relheight=0.03)
+root.mainloop()
 
-    card1 = Button(text='card1', image=suit)
-    card1.place(relx=x, rely=y+0.04, relwidth=0.04, relheight=0.13)
-
-    card2 = Button(text='card2', image=suit)
-    card2.place(relx=x+0.05, rely=y+0.04, relwidth=0.04, relheight=0.13)
-
-    bet_bank = Button(text='bet', state='disable')
-    bet_bank.place(relx=x+0.11, rely=y+0.02, relwidth=0.04, relheight=0.03)
-
-    raise_bet = Button(text='raise bet', command=raise_bet_click, state='disable')
-    raise_bet.place(relx=x+0.11, rely=y+0.06, relwidth=0.04, relheight=0.03)
-
-    pass_button = Button(text='pass', command=pass_button_click, state='disable')
-    pass_button.place(relx=x+0.11, rely=y+0.1, relwidth=0.04, relheight=0.03)
-
-    check = Button(text='check', command=check_click, state='disable')
-    check.place(relx=x+0.11, rely=y+0.14, relwidth=0.04, relheight=0.03)
-
-    user = Button(text='user '+user)
-    user.place(relx=x, rely=y+0.19, relwidth=0.09, relheight=0.03)
-
-    return {'bank': bank, 'raise_bet': raise_bet, 'bet': bet_bank, 'card1': card1,
-            'card2': card2, 'check': check, 'user': user}
+print("it never print")
 
 
-cards_center = []
-croupier = {'bank': 0, 'cards': cards_center}
-player_places = []
-size = 0.25
-delta_center = 0
-cards_center = add_card_on_center(form1, size+delta_center)
 
 
-position = 3
-shift = 0
-
-x_default = 0.02
-y_default = 0.75
-user_name = {'1': 'player 1', '2': 'user 2', '3': 'player 3', '4': 'player 4', '5': 'player 5', '6': 'player 6', '7':
-                    'player 7' }
-
-player_places.append(place_for_user(user_name.get('1'), x_default, y_default-0.6))
-player_places.append(place_for_user(user_name.get('2'), x_default, y_default-0.3))
-player_places.append(place_for_user(user_name.get('3'), x_default, y_default))
-player_places.append(place_for_user(user_name.get('4'), x_default+0.3, y_default))
-player_places.append(place_for_user(user_name.get('5'), x_default+0.6, y_default))
-player_places.append(place_for_user(user_name.get('6'), x_default+0.6, y_default-0.3))
-player_places.append(place_for_user(user_name.get('7'), x_default+0.6, y_default-0.6))
-
-#example for add some cards for the second player
-# this wil be dict, where number of range of card will be map to face's card
-image = Image.open("image//king_black.jpg")
-suit3 = ImageTk.PhotoImage(image)
-image = Image.open("image//five_red.jpg")
-suit4 = ImageTk.PhotoImage(image)
-
-player_places[1].get('card1').config(image=suit3)
-player_places[1].get('card2').config(image=suit4)
-card = (croupier.get('cards'))[0]
-card.config(image=suit3)
-
-form1.mainloop()
-
-#case with add new card in dynamic
-#this code will be work only with new thred
-# will be tinking
-sleep(0.01)
-cards_center = add_card_on_center(form1, size+0.02)
-card = (croupier.get('cards'))[1]
-card.config(image=suit3)
-print('ok')
-form1.mainloop()

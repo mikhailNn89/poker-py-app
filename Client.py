bf091d498da18a48619a5641b59b672638a784ff
Kuznetsov.A <mydumpster@mail.ru>
2018-10-22 01:30:51 +0300

    Add end_game

--- Client.py
+++ Client.py
@@ -2,120 +2,88 @@ import socket
 import threading
 import pickle
 
+testmode=False
+
 class Client(object):
-	def __init__(self, ip, port, username, myGui):
+	def __init__(self):
 		print('Hello Client')
-		self.username=username
+		self.username=""
 		self.count_max_raise = 0
-		self.myGui = myGui
+		self.is_connected=False
 		self.my_socket = socket.socket()
-		self.adapt_card = {'11': 'jack', '12': 'queen', '13': 'king', '14': 'ace'}
-		self.connect(ip,port)
+		#self.adapt_card = {'11': 'jack', '12': 'queen', '13': 'king', '14': 'ace'} #или тут таки адаптировать
+		#Функции =======================
+		#или даже типа сигналы
+		self.update=0
+		self.allow_to_step=0
+		self.game_has_begun=0
+		self.whose_step=0
+		self.end_game=0
+		self.invite_to_game=0
+		#==============================
 
 	def load_data(self):
-		return pickle.loads(self.my_socket.recv(200))
-
+		return pickle.loads(self.my_socket.recv(500))
+		
 	def send_data(self,data):
 		self.my_socket.send(pickle.dumps(data))
 
-	def connect(self,ip,port):
+	def connect(self,ip,port,username):
 		self.my_socket.connect((ip, port))
-		self.send_data({'name': self.username})
+		self.send_data({'name': username})
 		data = self.load_data()
 		print(data)
 		if data['status'] == 'ok':
 			self.my_id = data['yourid']
-			self.myGui.myId = self.my_id
-			self.myGui.change_name_user(self.my_id, self.username)
+			self.username=username
 			self.my_thread = threading.Thread(target=self.listener)
+			self.is_connected=True
 			self.my_thread.start()
 		else:	
 			#тут можно выкидывать какую-нибудь ошибку
 			pass
-	
+			
+	def get_id(self):
+		return self.my_id
+		
+	def get_username(self):
+		return self.username
+		
 	def listener(self):
 		while True:
-			data_from_server = pickle.loads(self.my_socket.recv(200))
+			data_from_server = pickle.loads(self.my_socket.recv(500))
 			print(data_from_server)
-			if data_from_server['action'] == 'step' and data_from_server['id'] == self.my_id:
-				
-				for i in self.myGui.player_places:#даем игроку возможность ходить. пока так 
-					if i['user']['text'] == self.username:
-						i['raise_bet']['state']='normal'
-					
-				step = self.do_step(data_from_server['id']) #do_step() функция, которая возвращает список из действия играок(1) и суммы(2)
-				#self.send_data({'action': 'move', 'move': step[0], 'count': step[1]})
+			if data_from_server['action'] == 'step':# and data_from_server['id'] == self.my_id:
+				if data_from_server['id'] == self.my_id:
+					self.allow_to_step()
+				self.whose_step(data_from_server)
+				if testmode:				
+					step = self.do_step(data_from_server['id']) #do_step() функция, которая возвращает список из действия играок(1) и суммы(2)
+					self.send_data({'action': 'move', 'move': step[0], 'count': step[1]})
 			elif data_from_server['action'] == 'update':
-				self.update(data_from_server)
+				self.set_bank(data_from_server['bank']) #установка у клиента текущего банка
+				self.players=data_from_server['gamers']
+				self.update(data_from_server)#это типа сигнал
+			elif data_from_server['action'] == 'end':
+				self.end_game(data_from_server)
 			elif data_from_server['action'] == 'start?':
-				self.send_data({'action': 'start?', 'answer': def_invite(data_from_server['players'])})  # def_invite функция, которая предлогает игроку начать игру, возвращает True/False
+				 self.invite_to_game();
 			elif data_from_server['action'] == 'start_game':
-				self.set_bank_of_player(data_from_server['money'])
-				self.set_card_on_player(data_from_server['cards'])
+				self.game_has_begun(data_from_server)
 				self.send_data({'action': 'ok'})
 			elif data_from_server['action'] == 'end_game':
 				pass
 				
-	def update(self,data):
-		self.set_bank(data['bank']) #установка у клиента текущего банка
-		print(data)
-		for player in data['gamers']:
-			print(player)
-			id = player[0]
-			curr_bet = player[1]
-			player_bank = player[2]
-			
-			#self.myGui.change_name_user(id, player[3])# пока так. чтобы у других игроков тоже были имена
-			
-			self.set_money(id, curr_bet, player_bank)
-			self.set_min_raise(data['to_raise']) #становка минимальной суммы для повышения ставки
-			self.set_open_cards(data['open_cards'], data['bank'])
-			
-	def adapt_cards(self, num_card):
-		if num_card <= 10:
-			return str(num_card)
-		else:
-			return self.adapt_card.get(str(num_card))
-
-	def set_card_on_player(self, cards):
-		card0 = cards[0]
-		num_card0 = self.adapt_cards(card0[0])
-		suit_card0 = (card0[1])[:-1]
-		card1 = cards[1]
-		num_card1 = self.adapt_cards(card1[0])
-		suit_card1 = (card1[1])[:-1]
-		self.myGui.set_card_one_player(self.myGui.getMyId(), suit_card0, num_card0, suit_card1, num_card1)
-
-	def set_bank_of_player(self, money):
-		self.myGui.add_bank_player(self.myGui.getMyId(), money)
-	
-	# curr_bet
-	def set_money(self, id, curr_bet, player_money):
-		self.myGui.add_bank_player(id, player_money)
-		self.myGui.change_bet_player(id, curr_bet)
-
-	def set_min_raise(self, min_bet):
-		pass
-
-	def set_open_cards(self, open_cards, bank):
-		if len(open_cards) != 0:
-			self.myGui.clean_card_on_center()
-			for card in open_cards:
-				num_card = self.adapt_cards(card[0])
-				suit_card = (card[1])[:-1]
-				self.myGui.change_croupier(bank, suit_card, num_card)
-				
+	def start_game(self):
+		self.send_data({'action': 'start?', 'answer': True})
+		
 	def step(self,data):
 		self.send_data({'action': 'move', 'move': data[0], 'count': data[1]})
-		for i in self.myGui.player_places:#даем игроку возможность ходить. пока так 
-			if i['user']['text'] == self.username:
-				i['raise_bet']['state']='disable'
-		
+
 	def do_step(self, id):
-		if id == 0:
+		if id == self.my_id:
 			return ['raise', 100]
-		else:
-			return ['call', 100]
+		
 		#if self.count_max_raise > 2:
 		#	self.count_max_raise = 0
 		#	return ['call', 100]
@@ -125,6 +93,7 @@ class Client(object):
 
 	def set_bank(self,money):
 		pass
+	
 
 
 
@@ -137,11 +106,7 @@ class Client(object):
 
 
 
-def def_invite(players):
-	if players >= 2:
-		return True
-	else:
-		return False
+
 
 
 if __name__ == '__main__':
--- Table_main.py
+++ Table_main.py
@@ -10,6 +10,15 @@ class GUI:
 		THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 		self.myId = -100
 		self.form1 = form
+		
+		self.client=Client()
+		self.client.update=self.update_data#когда приходит обновление в клиент, то дергается эта функция
+		self.client.allow_to_step=self.allow_to_step
+		self.client.game_has_begun=self.begin_game
+		self.client.end_game=self.end_game
+		self.client.invite_to_game=self.invite_to_game
+		self.client.whose_step=self.whose_step
+		
 		self.form1.title('form1')
 		self.form1.resizable(width=FALSE, height=FALSE)
 		self.form1.geometry('1200x662+100+100')
@@ -29,6 +38,8 @@ class GUI:
 		self.server_port.place(relx=0.40, rely=0.05, relwidth=0.04, relheight=0.03)
 		self.user_connect = Button(text='connect', command=self.connect_click)
 		self.user_connect.place(relx=0.45, rely=0.05, relwidth=0.04, relheight=0.03)
+		self.startbtn = Button(text='start', command=self.start_game, state="disable")
+		self.startbtn.place(relx=0.45, rely=0.02, relwidth=0.04, relheight=0.03)
 		self.cards_center = []
 		self.bank = Button(text='bank of casino')
 		self.croupier = {'bank': self.bank, 'cards': self.cards_center}
@@ -76,11 +87,15 @@ class GUI:
 		bank.place(relx=x, rely=y, relwidth=0.09, relheight=0.03)
 		#self.bank = tkinter.Entry(self.form1, textvariable="empty")
 		#self.bank.place(relx=x, rely=y, relwidth=0.09, relheight=0.03)
-
+		info_text = Button(text='', state='disable')#для вывода информации о шаге или победе
+		info_text.place(relx=x + 0.01, rely=y - 0.04, relwidth=0.1, relheight=0.03)#
 		card1 = Button(text='card1', image=self.shirt)
 		card1.place(relx=x, rely=y + 0.04, relwidth=self.weigh_card, relheight=self.height_card)
 		card2 = Button(text='card2', image=self.shirt)
 		card2.place(relx=x + 0.05, rely=y + 0.04,  relwidth=self.weigh_card, relheight=self.height_card)
+		curr_bet_text = StringVar()#
+		curr_bet= tkinter.Entry(self.form1, textvariable=curr_bet_text)#
+		curr_bet.place(relx=x + 0.11, rely=y + 0.00, relwidth=0.04, relheight=0.03)#
 		bet_bank = Button(text='0', state='disable')
 		bet_bank.place(relx=x + 0.11, rely=y + 0.02, relwidth=0.04, relheight=0.03)
 		raise_bet = Button(text='raise bet', command=self.raise_bet_click, state='disable')
@@ -92,7 +107,7 @@ class GUI:
 		user = Button(text='user ' + user)
 		user.place(relx=x, rely=y + 0.19, relwidth=0.09, relheight=0.03)
 		return {'bank': bank, 'raise_bet': raise_bet, 'bet': bet_bank, 'card1': card1,
-				'card2': card2, 'check': check, 'user': user, 'pass': pass_button}
+				'card2': card2, 'check': check, 'user': user, 'pass': pass_button,'curr_bet':curr_bet_text,'info':info_text,'id':-1}
                 
 	def setting_player_places(self):
 		x_default = 0.02
@@ -136,7 +151,20 @@ class GUI:
 		#    (self.player_places[position].get('bank'))['text'] = bank
 		#if int(bank) > 0:
 		#    (self.player_places[position].get('bank'))['text'] = (int(bank) - int(bet))
-				
+	
+	def start_game(self):
+		self.startbtn['text']= "Ждём других"
+		self.client.start_game()
+	
+	def whose_step(self,data):
+		for i in self.player_places:
+			if i['id'] == data['id'] and i['id']!=self.client.my_id:
+				i['info']['text']="Ход"
+				break
+	
+	def invite_to_game(self):
+		self.startbtn['state']='normal'
+		
 	def add_bank_player(self,position, summa):
 		if summa > 0:
 			(self.player_places[position].get('bank'))['text'] = summa
@@ -145,19 +173,104 @@ class GUI:
 		(self.player_places[position].get('user'))['text'] = name
 	
 	def raise_bet_click(self):
-		self.cl.step(('raise',100))#для теста
+		for i in self.player_places:#забираем возможность делать ход
+			if i['id'] == self.client.my_id:
+				i['raise_bet']['state']='disable'
+				i['info']['text']=''
+		for i in self.player_places:
+			if i['user']['text'] == self.client.username:
+				self.client.step(('raise',int(i['curr_bet'].get())))#для теста
+				
 
 	def check_click(self):
-		pass
+		self.client.step(('check',0))
 
 	def pass_button_click(self):
-		pass
-
-	def connect_click(self):
-		self.cl = Client(self.user_ip.get(), int(self.server_port.get()), self.user_name.get(), self)
+		self.client.step(('fold',0))
 		
+	def begin_game(self,data):
+		self.startbtn['text']= "Идёт игра"
+		self.set_bank_of_player(data['money'])
+		self.set_card_on_player(data['cards'])
+		
+	def connect_click(self):
+		self.client.connect(self.user_ip.get(), int(self.server_port.get()),self.user_name.get())
+		if self.client.is_connected:
+			self.myId = self.client.get_id()
+			self.change_name_user(self.client.get_id(), self.user_name.get())
+			self.set_userid(self.client.get_id())#пока так
+			
+	def allow_to_step(self):
+		for i in self.player_places:#даем игроку возможность ходить. пока так 
+			if i['id'] == self.client.my_id:
+				i['raise_bet']['state']='normal'
+				i['info']['text']='Твой ход'
+			else:
+				i['info']['text']=''
+		
+	def end_game(self,data):
+		if self.client.my_id==data['winners'][0]:
+			for i in self.player_places:
+				if i['id'] == data['winners'][0]:
+					i['info']['text']= "Ты победил! "+data['combination']
+		else:
+			for i in self.player_places:
+				if i['id'] == data['winners'][0]:
+					i['info']['text']= "Победа. "+data['combination']
+					
+	def update_data(self,data):
+		print(data)
+		for player in self.client.players:
+			print(player)
+			id = player[0]
+			curr_bet = player[1]
+			player_bank = player[2]
+			self.set_money(id, curr_bet, player_bank)
+			self.change_name_user(id, player[3])#пока так
+			self.set_userid(id)#пока так
+			self.set_min_raise(data['to_raise']) #становка минимальной суммы для повышения ставки
+			self.set_open_cards(data['open_cards'], data['bank'])
+			
+	def set_money(self, id, curr_bet, player_money):
+		self.add_bank_player(id, player_money)
+		self.change_bet_player(id, curr_bet)
+			
+	def set_min_raise(self, min_bet):
+		pass
+		
+	def set_userid(self,id):
+		position=id#пока так
+		self.player_places[position]['id']=id
+		#print((self.player_places[position].get('user'))['id'])
+		#(self.player_places[position].get('user'))['id'] = id
+		
+	def set_open_cards(self, open_cards, bank):
+		if len(open_cards) != 0:
+			self.clean_card_on_center()
+			for card in open_cards:
+				num_card = self.adapt_cards(card[0])
+				suit_card = (card[1])[:-1]
+				self.change_croupier(bank, suit_card, num_card)
+				
+	def adapt_cards(self, num_card):
+		self.adapt_card = {'11': 'jack', '12': 'queen', '13': 'king', '14': 'ace'}
+		if num_card <= 10:
+			return str(num_card)
+		else:
+			return self.adapt_card.get(str(num_card))
+			
+	def set_card_on_player(self, cards):
+		card0 = cards[0]
+		num_card0 = self.adapt_cards(card0[0])
+		suit_card0 = (card0[1])[:-1]
+		card1 = cards[1]
+		num_card1 = self.adapt_cards(card1[0])
+		suit_card1 = (card1[1])[:-1]
+		self.set_card_one_player(self.getMyId(), suit_card0, num_card0, suit_card1, num_card1)
 
-
+	def set_bank_of_player(self, money):
+		self.add_bank_player(self.getMyId(), money)
+		
 root = Tk()
 my_gui = GUI(root)
 my_gui.shirt = my_gui.get_image('shirt', 'shirt')
